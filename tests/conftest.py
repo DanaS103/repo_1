3222b538f7b13da2a05a52149e9ef8de3903088b
DevYeop <lee.sangyeop0715@gmail.com>
2026-01-09 05:10:21 +0900

    chore : 로컬 api url 추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,8 @@ import pytest
 def base_url():
     # jsonplaceholder base url
     return "https://jsonplaceholder.typicode.com"
+
+@pytest.fixture(scope="session")
+def base_url_local():
+    # jsonplaceholder base url
+    return "http://localhost:5000"
